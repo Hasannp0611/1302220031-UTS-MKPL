af72cdc95e2444660d4c3c1164c3cbab76521ee7
Hasannp0611 <hasannp0611@gmail.com>
2025-04-17 00:58:53 +0700

    Memperbaiki branch long parameter

--- src/main/java/com/mycompany/taxcalculator/EmpDetails.java
+++ src/main/java/com/mycompany/taxcalculator/EmpDetails.java
@@ -3,75 +3,24 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.mycompany.taxcalculator;
-
 /**
  *
  * @author hasannp
  */
 public class EmpDetails {
-    private String employeeId;
-    private String firstName;
-    private String lastName;
-    private String idNumber;
-    private String address;
-    private boolean Foreigner;
-    private boolean gender;
-    private int yearJoined;
-    private int monthJoined;
-    private int dayJoined;
+    private Info info;
+    private work work;
 
-    public EmpDetails(String employeeId, String firstName, String lastName, String idNumber, String address, boolean isForeigner, boolean gender, int yearJoined, int monthJoined, int dayJoined) {
-        this.employeeId = employeeId;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.idNumber = idNumber;
-        this.address = address;
-        this.Foreigner = isForeigner;
-        this.gender = gender;
-        this.yearJoined = yearJoined;
-        this.monthJoined = monthJoined;
-        this.dayJoined = dayJoined;
+    public EmpDetails(Info info, work work) {
+        this.info = info;
+        this.work = work;
     }
 
-    public String getEmployeeId() {
-        return employeeId;
+    public Info getInfo() {
+        return info;
     }
 
-    public String getFirstName() {
-        return firstName;
+    public work getWork() {
+        return work;
     }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public String getIdNumber() {
-        return idNumber;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public boolean isForeigner() {
-        return Foreigner;
-    }
-
-    public boolean isGender() {
-        return gender;
-    }
-
-    public int getYearJoined() {
-        return yearJoined;
-    }
-
-    public int getMonthJoined() {
-        return monthJoined;
-    }
-
-    public int getDayJoined() {
-        return dayJoined;
-    }
-    
-    
 }
